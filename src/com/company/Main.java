9f98759d9f784cbb6de0b19ee9d8afb31ea4432e
AlexGarkaviy <vox01@mail.ru>
2017-01-12 17:15:54 +0200

    Labs 141

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 981982394;
+        float e =  2.12f;
+        double f = 1.14;
+        boolean s = a == b;
+        System.out.println("byte "+ a +"\n"+"short " + b+"\n" + "int " + c+"\n"
+                + "long " + d+"\n" + "float " + e+"\n" + "double " + f + "\n"
+                + "boolean "+ s );
+
+        
+        System.out.println();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(s);
+    }
+}
